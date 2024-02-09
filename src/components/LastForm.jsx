bc2f8d0f2308fe38035f126f255c8c8961bcbe2f
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-02-09 19:16:00 +0100

    Update LastForm.jsx

--- src/components/LastForm.jsx
+++ src/components/LastForm.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import { StyledLayoutContent } from '../styled-components/containers'
+import { Button } from '@mui/material'
+import { APP_DATA } from '../CONSTANTS'
+import { useNavigate } from 'react-router-dom'
 
 export default function LastForm() {
+  const navigate = useNavigate()  
+
+  const handleSeeResults = () => {
+    navigate('/end-game')
+  }
+  
   return (
-    <div>LastForm</div>
+  <>
+    <StyledLayoutContent>
+      <h2>{APP_DATA.APP_LAST_FORM.TITLE}</h2>
+      <p>{APP_DATA.APP_LAST_FORM.SUBTITLE}</p>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+    </StyledLayoutContent>
+    <Button
+      variant='contained'
+      onClick={handleSeeResults}
+    >
+      {APP_DATA.APP_BUTTON_END}
+    </Button>
+  </>
   )
 }
