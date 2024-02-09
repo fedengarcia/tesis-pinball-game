a246d38ddcab376ec26b3be8fcde92f651c22b3e
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-02-09 17:53:06 +0100

    Create CONSTANTS.js

---
+++ src/CONSTANTS.js
@@ -0,0 +1,48 @@
+export const APP_DATA = {
+    APP_TITLE: "Bienvenido a mi tesis",
+    APP_SUBTITLE: "Espero que lo disfruten y se diviertan mientras aprendemos",
+    APP_BUTTON_PLAY: 'Responder y a jugar !',
+    APP_FIRST_FORM: {
+        TITLE: 'Titulo del primer formulario',
+        SUBTITLE: 'Subtitlo de primer formulario',
+        QUESTIONS:[
+        {
+            QUESTION: "Ejemplo de pregunta",
+            POSSIBLE_ANSWERS: ['Option 1', 'Option 2', 'Option 3']
+        },
+        {
+            QUESTION: "Ejemplo de pregunta",
+            POSSIBLE_ANSWERS: ['Option 1', 'Option 2', 'Option 3']
+        },
+        {
+            QUESTION: "Ejemplo de pregunta",
+            POSSIBLE_ANSWERS: ['Option 1', 'Option 2', 'Option 3']
+        },
+        // You can add here all the questions that you need
+        ]
+    },
+    APP_LAST_FORM:{
+        TITLE: 'Titulo del primer formulario',
+        SUBTITLE: 'Subtitlo de primer formulario',
+        QUESTIONS:[
+        {
+            QUESTION: "Ejemplo de pregunta",
+            POSSIBLE_ANSWERS: ['Option 1', 'Option 2', 'Option 3']
+        },
+        {
+            QUESTION: "Ejemplo de pregunta",
+            POSSIBLE_ANSWERS: ['Option 1', 'Option 2', 'Option 3']
+        },
+        {
+            QUESTION: "Ejemplo de pregunta",
+            POSSIBLE_ANSWERS: ['Option 1', 'Option 2', 'Option 3']
+        },
+        // You can add here all the questions that you need
+        ]
+    },
+    APP_PINBALL: {
+        TITLE: 'Juega al pinball',
+        POINTS: 'Puntos del juego',
+        BALLS: 'Te quedan ${quantity} bolas'
+    }
+}
