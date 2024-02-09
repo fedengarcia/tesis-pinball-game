1df45db511453ebaef1215dcedc731de69404d54
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-02-09 18:57:36 +0100

    Update FirstForm.jsx

--- src/components/FirstForm.jsx
+++ src/components/FirstForm.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import { StyledLayoutContent } from '../styled-components/containers'
+import { Button } from '@mui/material'
+import { APP_DATA } from '../CONSTANTS'
+import { useNavigate } from 'react-router-dom'
+
 
 export default function FirstForm() {
+  const navigate = useNavigate()  
+  
+  const handlePinballInit = () => {
+    navigate('pinball-game')
+  }
+  
   return (
-    <div>FirstForm</div>
+  <>
+    <StyledLayoutContent>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+    </StyledLayoutContent>
+    <Button
+      variant='contained'
+      onClick={handlePinballInit}
+    >
+      {APP_DATA.APP_BUTTON_PLAY}
+    </Button>
+  </>
   )
 }
