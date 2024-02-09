7989e9389693f905aa19d57a7593cb8c93cbf478
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-02-09 19:16:02 +0100

    Create PinballGameComponent.jsx

---
+++ src/components/PinballGameComponent/PinballGameComponent.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { APP_DATA } from '../../CONSTANTS'
+import { Button } from '@mui/material'
+import { StyledLayoutContent } from '../../styled-components/containers'
+
+export default function PinballGameComponent({
+    setIsGameEnd
+}) {
+
+  return (<>
+    <h1>{APP_DATA.APP_PINBALL.TITLE}</h1>
+    <StyledLayoutContent>
+        <div>PinballGameComponent</div>
+    </StyledLayoutContent>
+    <Button
+      variant='contained'
+      onClick={() => setIsGameEnd(true)}
+    >
+        {APP_DATA.APP_PINBALL.END_GAME_BUTTON}
+    </Button>
+  </>
+  )
+}
