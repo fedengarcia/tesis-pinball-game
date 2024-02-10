0037b5887aaef8ec32d528b639b2b071b5472bfe
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-02-10 10:30:49 +0100

    Update PinballGame.jsx

--- src/pages/PinballGame.jsx
+++ src/pages/PinballGame.jsx
@@ -1,18 +1,10 @@
 import React, { useState } from 'react'
-import { StyledAppLayout, StyledLayoutContent } from '../styled-components/containers'
-import { APP_DATA } from '../CONSTANTS'
+import { StyledAppLayout } from '../styled-components/containers'
 import PinballGameComponent from '../components/PinballGameComponent/PinballGameComponent'
-import { Button } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
 import LastForm from '../components/LastForm'
 
 export default function PinballGame() {
   const [isGameEnd, setIsGameEnd] = useState(false)
-  const navigate = useNavigate()
-
-  const handlePinballEnd = () => {
-    setIsGameEnd(true)
-  }
 
   return (
     <StyledAppLayout>
