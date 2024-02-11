3b2c050cd73db05f9a4b5917a89b20f78cae9b0e
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-02-11 19:47:10 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { StyledAppLayout } from './styled-components/containers'
+import { StyledAppLayout, StyledLayoutContent } from './styled-components/containers'
 import { APP_DATA } from './CONSTANTS'
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import { Button, Input, TextField, InputAdornment, Box } from '@mui/material'
+
 import FirstForm from './components/FirstForm'
+import { useEffect, useState } from 'react';
+import Login from './components/Login';
 
 export default function App() {
+  const [isLogged, setIsLogged]= useState(false);
+
+  const handleLogin = () => {
+    setIsLogged(true)
+  }
+
+
 
   return (
     <StyledAppLayout>
       <h1>{APP_DATA.APP_TITLE}</h1>
 
-      <FirstForm/>
+      {!isLogged ? 
+        <Login handleLogin={handleLogin}/>
+      :
+        <FirstForm/>
+      }
+
     </StyledAppLayout>
   )
 }
