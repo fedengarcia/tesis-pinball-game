2dfec831375120e06a0ab509457f18319f60c1bc
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-02-11 20:21:32 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import Routes from './routes/Routes'
+import { UserProvider } from './UserProvider/UserProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <UserProvider>
       <Routes />
+
+    </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
