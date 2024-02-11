1e309e1919192a1a5ed707f3658fbedecd5a2e6f
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-02-11 23:14:36 +0100

    Update UserProvider.jsx

--- src/UserProvider/UserProvider.jsx
+++ src/UserProvider/UserProvider.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import UserContext from './UserContext';
 import { APP_DATA } from '../CONSTANTS';
+import { createUserDocument, getUserByEmail } from '../firebase/firebase';
 
 export const UserProvider = ({children}) => {
     const [isLogged, setIsLogged]= useState(false);
@@ -9,26 +10,35 @@ export const UserProvider = ({children}) => {
     })
 
 
-    
     const validateEmail = (email) => {
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
         const validation = regex.test(email);
         return validation
     }   
 
-    const handleLogin = () => {
-        let exist = false
+    const handleLogin = async () => {
         let userInfoCopy = {...userInfo}
-        if(exist){
-
+        const userResponse = await getUserByEmail(userInfo.email)
+        console.log(userResponse)
+        // Check email in firebase
+        if(userResponse){
+            userInfoCopy = {...userResponse}
+            console.log("Ya existe, toma ==>", userInfoCopy)
         }else{
             let tables = APP_DATA.APP_GAME.GAME_CONFIGURATION.tables
             userInfoCopy.tableAssigned = tables[Math.floor(Math.random() * tables.length)]
             userInfoCopy.date = new Date();
             userInfoCopy.totalPoints = 0
-            userInfoCopy.gameTime = 0
+            userInfoCopy.timePlayed = 0
+            userInfoCopy.firstForm = {
+                isCompleted: false
+            }
+            userInfoCopy.finalForm = {
+                isCompleted: false
+            }
+            console.log("Creado !!!", userInfoCopy)
+            await createUserDocument(userInfoCopy)
         }
-        // Check email in firebase
         setUserInfo(userInfoCopy)
         setIsLogged(true)
     }
