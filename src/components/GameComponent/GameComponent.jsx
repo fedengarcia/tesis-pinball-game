2558d9c7d70b4c3a4e554848b610dbd45f670205
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-02-11 23:48:38 +0100

    Update GameComponent.jsx

--- src/components/GameComponent/GameComponent.jsx
+++ src/components/GameComponent/GameComponent.jsx
@@ -1,20 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { APP_DATA } from '../../CONSTANTS'
 import { Button } from '@mui/material'
 import { StyledLayoutContent } from '../../styled-components/containers'
 import Pinball from '../Pinball/Pinball'
+import { editUser } from '../../firebase/firebase'
+import UserContext from '../../UserProvider/UserContext'
 
 export default function GameComponent({
     setIsGameEnd,
-    setGameResult,
-    gameResult
   }) {
+    const {userInfo, setUserInfo} = useContext(UserContext)
     const [buttonDisabled, setButtonDisabled] = useState(true)
+    const [gameResult, setGameResult] = useState({
+      points: null
+    })
 
     useEffect(() => {
       console.log("GAME RESULT", gameResult)
       if(gameResult.points !== null) setButtonDisabled(false)
     }, [gameResult]);
+
+    const handleEndGame = async () => {
+      let userInfoCopy = {...userInfo}
+      userInfoCopy.gameCompleted = true
+      const userUpdated = await editUser(userInfo.email, userInfoCopy)
+      if(userUpdated){
+        setUserInfo(userInfoCopy)
+        setIsGameEnd(true)
+      }else{
+        alert("Err updating data")
+      }
+    }
 
   return (<>
     <h1>{APP_DATA.APP_GAME.TITLE}</h1>
@@ -26,8 +42,8 @@ export default function GameComponent({
     </StyledLayoutContent>
     <Button
       variant='contained'
-      disabled={buttonDisabled}
-      onClick={() => setIsGameEnd(true)}
+      // disabled={buttonDisabled}
+      onClick={handleEndGame}
     >
         {APP_DATA.APP_GAME.END_GAME_BUTTON}
     </Button>
