fe0b257e936737dc1c5c50c1aa51f44b1c804c18
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-02-14 00:46:16 +0100

    Update Bricks.jsx

--- src/components/Bricks/Bricks.jsx
+++ src/components/Bricks/Bricks.jsx
@@ -150,7 +150,7 @@ export default function Bricks({typeTableGame}){
     if(bricks.length!==0){
         update();
     }
-  }, [bricks,canvas]);
+  }, [bricks]);
 
   function update() {
     movePaddle();
