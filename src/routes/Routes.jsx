05a16830c54417bac775e11000684fb31cb7b0e3
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-02-15 21:42:43 +0100

    update routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,16 +3,26 @@ import { Routes as ReactRoutes, Route } from 'react-router-dom';
 import App from '../App'
 import Game from '../pages/Game';
 import EndGame from '../pages/EndGame';
+import FirstForm from '../pages/FirstForm';
+import LastForm from '../pages/LastForm';
 
 export default function Routes () {
+
+
     return (
         <ReactRoutes >
             <Route
                 exact path={'/'}
                 element={<App/>} />
             <Route
+                exact path={'/first-form'}
+                element={<FirstForm/>} />
+            <Route
                 exact path={'/game'}
                 element={<Game/>} />
+            <Route
+                exact path={'/final-form'}
+                element={<LastForm/>} />
             <Route
                 exact path={'/end-game'}
                 element={<EndGame/>} />
