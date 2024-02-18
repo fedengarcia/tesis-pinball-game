8902e62265b8df156107828dd20b6192fe2e0a2d
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-02-18 11:02:06 +0100

    Create useSweetAlert.jsx

---
+++ src/hooks/useSweetAlert.jsx
@@ -0,0 +1,57 @@
+import Swal from "sweetalert2";
+// import './sweetAlertClasses.css'
+
+export default function useSweetAlert () {
+
+    const modal = async (text,title, onConfirmFunction) => {
+        Swal.fire({
+            text: text,
+            title: title,
+            showCancelButton: true,
+            confirmButtonText: 'Accept',
+            color: 'black',
+            confirmButtonColor: '#43a1a2',
+            background: 'rgb(255,255,255)',
+            focusConfirm: false,
+            customClass: {
+                popup: 'modalContainer',
+            }
+        }).then(async(result) => {
+            if (result.isConfirmed) {
+                onConfirmFunction()
+            }
+        })
+    }
+
+
+    const popUp = ({message, popUpPosition, timer, textColor, showProgressBar, props}) => {
+        return  <>
+        {Swal.fire({
+            toast: true,
+            title: message,
+            position: popUpPosition ?? 'bottom',
+            // icon: iconType,
+            showConfirmButton: false,
+            timer: timer ? timer : 2000,
+            timerProgressBar: showProgressBar ?? false,
+            color: textColor ?? 'black',
+            // iconColor: iconColor,
+            showClass: {
+                popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+                popup: 'animate__animated animate__fadeOutUp'
+            },
+            ...props,
+        })}
+        </>
+    }
+
+
+    return {
+        modal,
+        popUp
+    }
+}
+
+   
