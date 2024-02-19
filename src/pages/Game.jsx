535a80016383e9e520666e3eed8de5021b4be998
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-02-19 22:54:33 +0100

    Update Game.jsx

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -12,18 +12,20 @@ import useSweetAlert from '../hooks/useSweetAlert'
 
 export default function Game() {
   const navigate = useNavigate()
-  const {userInfo, setUserInfo, loadingLogin} = useContext(UserContext)
+  const {userInfo, setUserInfo, loadingLogin, setLoadingLogin} = useContext(UserContext)
   const [buttonDisabled, setButtonDisabled] = useState(true)
   const [gameConfiguration, setGameConfiguration] = useState([])
   const [gameResult, setGameResult] = useState(undefined)
   const [playingGame, setPlayingGame] = useState(false)
   const [gameStatus, setGameStatus] = useState('FIRST_TIME')
   const {popUp, modal} = useSweetAlert()
+  
 
   useEffect(() => {
+    console.log(userInfo?.gamesPlayed)
     if(userInfo?.gamesPlayed?.length === 0) setGameStatus('FIRST_TIME')
     if(userInfo?.gamesPlayed?.length === 1) setGameStatus('SECOND_TIME')
-    if(userInfo?.gamesPlayed?.length === 2) setGameStatus('GAME_FINISH')
+    if(userInfo?.gamesPlayed?.length >= 2) setGameStatus('GAME_FINISH')
   }, [userInfo]);
 
   useEffect(() => {
@@ -37,11 +39,6 @@ export default function Game() {
     if(gameResult?.points !== null) setButtonDisabled(false)
   }, [gameResult]);
 
-  const handleEndGame = async () => {
-    if(gameStatus === 'GAME_FINISH') navigate('/final-form')
-   
-    setPlayingGame(true)
-  }
 
   const showBonification = (message) =>{
     return popUp({
@@ -55,6 +52,7 @@ export default function Game() {
 
 
   const saveGameResults = async () => {
+    setLoadingLogin(true)
     let userInfoCopy = {...userInfo}
     userInfoCopy.gamesPlayed = [...userInfoCopy.gamesPlayed, gameResult]
     const userUpdated = await editUser(userInfo.email, userInfoCopy)
@@ -62,6 +60,7 @@ export default function Game() {
       setUserInfo(userInfoCopy)
     }
     setPlayingGame(false)
+    setLoadingLogin(false)
   }
 
   return (
@@ -81,16 +80,27 @@ export default function Game() {
                     />
                   }
                   {!playingGame && 
-                    <StyledFlexCenter direction="column" style={{height: '100%', alignItems:'center'}}>
+                    <StyledFlexCenter direction="column" style={{height: '100%', alignItems:'center', width: '50%'}}>
                       <h1>{APP_DATA.APP_GAME.GAME_STATUS[gameStatus].TITLE}</h1>
                       <h2>{APP_DATA.APP_GAME.GAME_STATUS[gameStatus].SUBTITLE}</h2>
                       <Button
+                        fullWidth
                         variant='contained'
                         // disabled={buttonDisabled}
-                        onClick={handleEndGame}
+                        onClick={() => setPlayingGame(true)}
                       >
                         {APP_DATA.APP_GAME.GAME_STATUS[gameStatus].BUTTON_TITLE}
                       </Button>
+                      {gameStatus === "GAME_FINISH" &&
+                      <Button
+                        style={{margin: '10px'}}
+                        variant='contained'
+                        fullWidth
+                        // disabled={buttonDisabled}
+                        onClick={() => navigate('/final-form')}
+                      >
+                        {userInfo.finalForm.isCompleted ? APP_DATA.APP_GAME.GAME_STATUS[gameStatus].BUTTON_TITLE3 : APP_DATA.APP_GAME.GAME_STATUS[gameStatus].BUTTON_TITLE2}
+                      </Button>}
                     </StyledFlexCenter>
                   }
               </>
