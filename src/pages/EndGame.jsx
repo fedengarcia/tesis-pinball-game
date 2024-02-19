d8de53fd1665afacbf9dac19f592f3465850b4a2
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-02-19 22:54:25 +0100

    Update EndGame.jsx

--- src/pages/EndGame.jsx
+++ src/pages/EndGame.jsx
@@ -7,36 +7,23 @@ import { editUser } from '../firebase/firebase';
 import { useNavigate } from 'react-router-dom';
 
 export default function EndGame() {
-  const {userInfo, setUserInfo} = useContext(UserContext)
   const navigate = useNavigate()
 
 
-  const handleRestartGame = async () => {
-    let userInfoCopy = {...userInfo}
-    userInfoCopy.gameCompleted = false
-    const userUpdated = await editUser(userInfo.email, userInfoCopy)
-    
-    if(userUpdated){
-      setUserInfo(userUpdated)
-      navigate('/game')
-    }else{
-      alert("Err updating data")
-    }
-    
-  }
     return (
       <StyledAppLayout>
-    <h1>{APP_DATA.APP_END.TITLE}</h1>
-    <StyledLayoutContent style={{padding: 0, overflow:'hidden'}}>
-      <h2>{APP_DATA.APP_END.SUB_TITLE}</h2>
-    </StyledLayoutContent>
-    <Button
-      variant='contained'
-      // disabled={buttonDisabled}
-      onClick={() => handleRestartGame()}
-    >
-        {APP_DATA.APP_END.RESTART_BUTTON}
-    </Button>
+        <h1>{APP_DATA.APP_END.TITLE}</h1>
+        <StyledLayoutContent style={{padding: 0, overflow:'hidden'}}>
+          <h2>{APP_DATA.APP_END.SUB_TITLE}</h2>
+
+          <Button
+            variant='contained'
+            // disabled={buttonDisabled}
+            onClick={() => navigate('/game')}
+          >
+              {APP_DATA.APP_END.RESTART_BUTTON}
+          </Button>
+        </StyledLayoutContent>
       </StyledAppLayout>
     )
   }
