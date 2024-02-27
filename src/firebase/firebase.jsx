da65974ef7d8361eaa851b88bd3f41d11fc16e5e
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-02-27 18:48:18 +0100

    Update firebase.jsx

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -49,10 +49,9 @@ export const getUserByEmail = async (email) => {
       if (!querySnapshot.empty) {
         // Obtener el primer documento encontrado (asumiendo que el campo "email" es único)
         const usuarioDoc = querySnapshot.docs[0];
-  
         // Obtener los datos del usuario
         const usuario = usuarioDoc.data();
-        return usuario;
+        return {...usuario, id: usuarioDoc.id};
       } else {
         console.log("No se encontró ningún usuario con ese correo electrónico.");
         return false
@@ -64,34 +63,73 @@ export const getUserByEmail = async (email) => {
   }
 
 
-
-  export const editUser = async (email, userDataUpdated) => {
-    console.log(email, userDataUpdated)
-    const usuariosCollection = collection(firebaseDB, "usuarios");
-    // Crear una consulta para buscar el usuario por su correo electrónico
-    const q = query(usuariosCollection, where("email", "==", email));
-  
-    try {
-      // Realizar la consulta
-      const querySnapshot = await getDocs(q);
-  
-      // Verificar si se encontró algún documento
-      if (!querySnapshot.empty) {
-        // Obtener el primer documento encontrado (asumiendo que el campo "email" es único)
-        const usuarioDoc = querySnapshot.docs[0];
-  
-        // Obtener la referencia al documento y actualizar los datos
-        await updateDoc(doc(usuariosCollection, usuarioDoc.id), userDataUpdated);
-        return userDataUpdated
-      } else {
-        console.log("No se encontró ningún usuario con ese correo electrónico.");
+  export const addNewGamePlayed = async (userData) => {
+    let gamePlayed = {
+        email: userData.email,
+        user_id: userData.id,
+    }
+    // Add to rank the best game of the first two
+    if(userDataUpdated.gamesPlayed.length === 2){
+      const rankingCollection = collection(firebaseDB, "ranking");
+      const newGameToRanking = doc(rankingCollection);
+      const bestGame = userData.gamesPlayed.sort((a, b) => b.score - a.score)[0];
+      gamePlayed = {
+        ...gamePlayed,
+        ...bestGame
+      }
+      try {
+          // Utilizar setDoc para agregar el nuevo documento con los datos del usuario
+          await setDoc(newGameToRanking, bestGame);
+      } catch (error) {
+          console.error("Error al agregar nuevo data juego:", error.message);
         return false
+      } 
+    }
+    // Add to gamePlayed collection
+    try {
+        gamePlayed = {
+          ...gamePlayed,
+          ...userData.gamesPlayed[userData.gamesPlayed.length - 1]
         }
+        const gamesPlayedCollection = collection(firebaseDB, "gamesPlayed");
+        const newGamePlayed = doc(gamesPlayedCollection);
+        // Utilizar setDoc para agregar el nuevo documento con los datos del usuario
+        await setDoc(newGamePlayed, gamePlayed);
     } catch (error) {
-      console.error("Error al editar usuario por correo electrónico:", error.message);
+        console.error("Error al agregar nuevo data ranking:", error.message);
         return false
     }
+    return true
+  } 
+
+
+
+export const editUser = async (userId, userDataUpdated) => {
+    console.log(userDataUpdated)
+
+  try {
+    const usuariosCollection = collection(firebaseDB, "usuarios");
+    
+    // Obtener la referencia al documento por su ID
+    const usuarioRef = doc(usuariosCollection, userId);
+    
+    // Verificar si el documento existe
+    const usuarioDoc = await getDoc(usuarioRef);
+
+    if (usuarioDoc.exists()) {
+      // Actualizar los datos del usuario
+      await updateDoc(usuarioRef, userDataUpdated);
+      let usuarioDoc = await getDoc(usuarioRef);
+      return usuarioDoc.data();
+    } else {
+      console.log("No se encontró ningún usuario con ese ID.");
+      return false;
+    }
+  } catch (error) {
+    console.error("Error al editar usuario por ID:", error.message);
+    return false;
   }
+}
   
   
 
