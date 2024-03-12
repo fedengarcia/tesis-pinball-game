d9f0d4cc77401a62ba5127883b11ff0f8baadaf8
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-03-12 18:07:44 +0100

    Update Game.jsx

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -19,6 +19,7 @@ export default function Game() {
   
 
   useEffect(() => {
+    console.log(userInfo)
     if(!userInfo.email) navigate('/')
     if(userInfo?.gamesPlayed?.length === 0) setGameStatus('FIRST_TIME')
     if(userInfo?.gamesPlayed?.length === 1) setGameStatus('SECOND_TIME')
@@ -54,6 +55,21 @@ export default function Game() {
     })
   }
 
+  const launchGameModal = () => {
+    modal(
+      APP_DATA.APP_GAME.CONTROLS.TITLE, 
+      `
+      <p>${APP_DATA.APP_GAME.CONTROLS.SUBTITLE}</p>
+      <p>${APP_DATA.APP_GAME.CONTROLS.SUBTITLE2}</p>
+      <p>${APP_DATA.APP_GAME.CONTROLS.SUBTITLE3}</p>
+      <p>${APP_DATA.APP_GAME.CONTROLS.SUBTITLE4}</p>
+      `,
+      () => setPlayingGame(true),
+      APP_DATA.APP_GAME.CONTROLS.BUTTON
+      )
+  }
+
+
   return (
     <StyledAppLayout>
       <h1>{APP_DATA.APP_GAME.TITLE}</h1>
@@ -71,26 +87,59 @@ export default function Game() {
                   }
                   {!playingGame && 
                     <StyledFlexCenter direction="column" style={{height: '100%', alignItems:'center', width: '50%'}}>
-                      <h1>{APP_DATA.APP_GAME.GAME_STATUS[gameStatus].TITLE}</h1>
-                      <h2>{APP_DATA.APP_GAME.GAME_STATUS[gameStatus].SUBTITLE}</h2>
+                      <div>
+                        {<h1>{APP_DATA.APP_GAME.GAME_STATUS[gameStatus].TITLE}</h1>}
+                      </div>
+                      
+                      
+                      <div>
+                        {!userInfo.finalForm.isCompleted && 
+                        <>
+                          <p>{APP_DATA.APP_GAME.GAME_STATUS[gameStatus].SUBTITLE}</p>
+                          <p>{APP_DATA.APP_GAME.GAME_STATUS[gameStatus].SUBTITLE2}</p>
+                          <p>{APP_DATA.APP_GAME.GAME_STATUS[gameStatus].ADVISE}</p>
+                          <p>{APP_DATA.APP_GAME.GAME_STATUS[gameStatus].ADVISE2}</p>
+                          </>
+                        }
+                       {APP_DATA.APP_GAME.GAME_STATUS[gameStatus].SUBTITLE3 &&
+                       <p>{APP_DATA.APP_GAME.GAME_STATUS[gameStatus].SUBTITLE3}</p>}
+                        {gameStatus === "GAME_FINISH" && userInfo.finalForm.isCompleted &&
+                          <>
+                              {APP_DATA.APP_GAME.GAME_STATUS[gameStatus].CANT_PLAY}
+                          </>
+                        
+                        }
+
+                      {!userInfo.finalForm.isCompleted && 
                       <Button
                         fullWidth
                         variant='contained'
                         // disabled={buttonDisabled}
-                        onClick={() => setPlayingGame(true)}
+                        onClick={launchGameModal}
                       >
                         {APP_DATA.APP_GAME.GAME_STATUS[gameStatus].BUTTON_TITLE}
-                      </Button>
-                      {gameStatus === "GAME_FINISH" &&
+                      </Button>}
+                      {gameStatus === "GAME_FINISH" && userInfo.finalForm.isCompleted &&
                       <Button
-                        style={{margin: '10px'}}
+                        style={{margin: '15px 0'}}
                         variant='contained'
                         fullWidth
                         // disabled={buttonDisabled}
                         onClick={() => navigate('/final-form')}
                       >
-                        {userInfo.finalForm.isCompleted ? APP_DATA.APP_GAME.GAME_STATUS[gameStatus].BUTTON_TITLE3 : APP_DATA.APP_GAME.GAME_STATUS[gameStatus].BUTTON_TITLE2}
+                        {APP_DATA.APP_GAME.GAME_STATUS[gameStatus].BUTTON_TITLE3}
                       </Button>}
+                      {gameStatus === "GAME_FINISH" && !userInfo.finalForm.isCompleted &&
+                      <Button
+                        style={{margin: '15px 0'}}
+                        variant='contained'
+                        fullWidth
+                        // disabled={buttonDisabled}
+                        onClick={() => navigate('/final-form')}
+                      >
+                        {APP_DATA.APP_GAME.GAME_STATUS[gameStatus].BUTTON_TITLE2}
+                      </Button>}
+                      </div>
                     </StyledFlexCenter>
                   }
               </>
