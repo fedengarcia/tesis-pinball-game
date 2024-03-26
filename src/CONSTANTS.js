e6349b24c537526d90c15b1507c7bbb031e82d73
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-03-26 19:40:19 +0100

    Update CONSTANTS.js

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -11,7 +11,7 @@ const detectBrowser = () => {
     if (userAgent.indexOf(BROWSERS.CHROME) !== -1) { //FUNCIONA PERFECTO !!
         return {
             addBlocksLineTime: 10000,
-            ballVelocity: 5,
+            ballVelocity: 8,
             ballColisionVelocitiy: 0.02,
             paddleVelocity: 6,
             paddleBofinificationDuration: 12000,
