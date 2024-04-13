2ce3719c5b79484ec20454344325c86e8843fa5a
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-04-13 09:20:32 +0200

    create second form

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,6 +6,7 @@ import EndGame from '../pages/EndGame';
 import FirstForm from '../pages/FirstForm';
 import LastForm from '../pages/LastForm';
 import Admin from '../pages/Admin';
+import SecondForm from '../pages/SecondForm';
 
 export default function Routes () {
 
@@ -15,6 +16,7 @@ export default function Routes () {
             <Route exact path={'/admin-javiT2024'} element={<Admin/>} />
             <Route exact path={'/'} element={<App/>} />
             <Route exact path={'/first-form'} element={<FirstForm/>} />
+            <Route exact path={'/second-form'} element={<SecondForm/>} />
             <Route exact path={'/game'} element={<Game/>} />
             <Route exact path={'/final-form'} element={<LastForm/>} />
             <Route exact path={'/end-game'} element={<EndGame/>} />
